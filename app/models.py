0061c31fc09698f142057d9c463f1a30d7b55b9a
MoodyBlues04 <105989507+MoodyBlues04@users.noreply.github.com>
2024-03-07 23:36:03 +0300

    fix

--- app/models.py
+++ app/models.py
@@ -4,7 +4,6 @@ from app.services.dto import TableRow
 
 
 class ParsedChannel(models.Model):
-    id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255, unique=True)
     subscribers = models.IntegerField()
     increment = models.IntegerField()
