a90cab2e39dfc249f6d17df551e78d2f7c6a2e51
MoodyBlues04 <105989507+MoodyBlues04@users.noreply.github.com>
2024-03-20 11:41:42 +0300

    test

--- app/services/tg_api.py
+++ app/services/tg_api.py
@@ -3,5 +3,5 @@ from django.conf import settings
 
 
 def is_channel_inactive(channel_tag: str) -> bool:
-    output = check_output(["python", str(settings.BASE_DIR) + "/telemetr_api.py", channel_tag])
+    output = check_output(["python3", str(settings.BASE_DIR) + "/telemetr_api.py", channel_tag])
     return bool(int(output))
